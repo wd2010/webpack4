fd4806737da0cb2ab911a9a85808553c406a3231
Dan Wen <dan.wen@ericsson.com>
2018-12-11 17:52:39 +0800

    修复babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
 
-const str = [1, 2, 3]
-str.includes(2)
+const arr=[1,2,3,4]
+arr.includes(1)
+
+
+
+const App =()=>
+  <div>
+    666
+  </div>
+
+render(<App />, document.getElementById('root'));
